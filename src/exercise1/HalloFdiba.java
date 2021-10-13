2f85799dd2aa5ca4ab1b561a9ef6270bb5c9829a
HNeychev <>
2021-10-13 15:28:21 +0000

    azisazisazis

--- src/exercise1/HalloFdiba.java
+++ src/exercise1/HalloFdiba.java
@@ -2,11 +2,8 @@ package exercise1;
 public class HalloFdiba {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hallo FDIBA!");
-=======
-        System.out.println("HalloFdiba");
->>>>>>> 9d86268e8625c90457cf12b5d6a6505b54ccf2b2
-    }
 
-}
+        System.out.println("Hallo FDIBA!");
+
+} 
+ }
--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -8,13 +8,6 @@ public class Tree {
         System.out.println(" *******");
         System.out.println("*********");
         System.out.println("   ***");
-<<<<<<< HEAD
-=======
-        System.out.println("Achtung! Achtung! Achtung!");
->>>>>>> 9d86268e8625c90457cf12b5d6a6505b54ccf2b2
-
-    
-    
     }
 
 }
